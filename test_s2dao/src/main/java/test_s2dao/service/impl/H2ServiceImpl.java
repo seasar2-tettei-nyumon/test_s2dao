69e212144c89f034d912a2f40c602684fa14562d
sano02601h <k.sano@a-too.co.jp>
2016-05-04 13:35:13 +0900

    mokurokuに近づけた。まだ完全に再現はできていない。

--- test_s2dao/src/main/java/test_s2dao/service/impl/H2ServiceImpl.java
+++ test_s2dao/src/main/java/test_s2dao/service/impl/H2ServiceImpl.java
@@ -17,12 +17,29 @@ public class H2ServiceImpl implements H2Service {
 	
 	//public Logger log = org.seasar.framework.log.Logger.getLogger(this.getClass());//●これってなんなんだろう。
 	
+	@Override
 	public H2TableBean settei(Integer id) {
 		H2TableBean bean = h2tableDao.selectById(id);
+		privatetest2(bean);
 		return bean;
 	}
 	
+	@Override
 	public int setteiMId(String a) {
 		return 1;
+	}
+	
+	//●あとでprivateにしてテストを書いてみる。いやpublicとprivateで書いてみよう。
+	//●ここで、このメソッドの中に、インジェクションのやつがあったから、外に追い出したんだな。ようやく原因を突き止めた。ようやく解決策を考える段階にいけた。
+	//●エラーにもならないからちゃんと知識がないとなんで動かないのかわからない。慎重に調査すること。
+	//●しかもこのメソッドは、インタフェース側で実装されていない。インジェクションをするのなら、インタフェース側にもメソッドを書いておかないとなのに。mokurokuもまったく同じことをやっていた。だからインジェクションが行われてなかったんだ。
+	public H2TableBean privatetest(Integer id){
+		H2TableBean bean = h2tableDao.selectById(id);	
+		return bean;
+	}
+	
+	//●フィールドのインジェクションを排除したメソッドを作った。●変わりに引数に材料を事前につめないといけなくなった。クラスのインジェクションされるフィールドが多いほどこのやり方だとしんどくなってくる。やっぱり、インジェクションに逆らわないほうがいいかな。
+	public H2TableBean privatetest2(H2TableBean bean){
+		return bean;
 	}
 }
--- test_s2dao/src/test/java/test_s2dao/util/Main2Test.java
+++ test_s2dao/src/test/java/test_s2dao/util/Main2Test.java
@@ -6,12 +6,14 @@ import static org.junit.Assert.*;
 import javax.annotation.Resource;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.seasar.framework.container.SingletonS2Container;
 import org.seasar.framework.container.factory.SingletonS2ContainerFactory;
 
 import test_s2dao.entity.H2TableBean;
 import test_s2dao.service.H2Service;
+import test_s2dao.service.impl.H2ServiceImpl;
 
 
 public class Main2Test {
@@ -25,6 +27,7 @@ public class Main2Test {
 		h2Service  = SingletonS2Container.getComponent("h2Service");	//Smart deployしたときの名前がこれらしい。●コンソールに自動登録したときに名前が出ているので参考にするように。
 	}
 
+	//@Ignore
 	@Test
 	public void daoのselectのテストの場合() throws Exception {
 		//========================
@@ -40,4 +43,45 @@ public class Main2Test {
 		//========================
 		assertThat(actual.getText() ,is("テストポスグレ"));
 	}
+	
+	//●すごくmokurokuのミニマルな状況に近づけることに成功した。すごく見やすくてビックリしている。何が起こっているのかだんだんわかってきた。
+	@Ignore("mokurokuのやりかた。はじめにぶつかった状態。失敗するので。")
+	@Test
+	public void mokurokuに近づけたテストの場合() throws Exception {
+		//========================
+		// SetUp - 初期化
+		//========================
+		H2ServiceImpl sut = new H2ServiceImpl();//●クラスを直接newしちゃっていた。いかん。
+		//========================
+		// Exercise - テストの実行
+		//========================
+		H2TableBean actual = sut.privatetest(1);//●前回はここでたくさん引数を渡すことになってしまっていた。今回は、インジェクションの知識をつけたので、それを解消するのを目的とする。
+		//========================
+		// Verify - 検証
+		//========================
+		assertThat(actual.getText() ,is("テストポスグレ"));
+	}
+	
+	//@Ignore("mokurokuのやりかた。排除した状態。")
+	@Test
+	public void mokurokuに近づけたテストの場合_排除した_現在mokurokuはこの状態() throws Exception {
+		//========================
+		// SetUp - 初期化
+		//========================
+		H2ServiceImpl sut = new H2ServiceImpl();//●クラスを直接newしちゃっていた。いかん。
+		H2TableBean   a   = new H2TableBean();
+		a.setId(1);
+		a.setText("テストだよ");
+		//========================
+		// Exercise - テストの実行
+		//========================
+		H2TableBean actual = sut.privatetest2(a);//●前回はここでたくさん引数を渡すことになってしまっていた。今回は、インジェクションの知識をつけたので、それを解消するのを目的とする。
+		//========================
+		// Verify - 検証
+		//========================
+		assertThat(actual.getText() ,is("テストだよ"));
+	}
+	
+	//●つまりインジェクションが出来ていれば、idとかキーの値をやればいいのだが、newしてしまったら、フィールドのdaoとかが使えないので、事前に準備しなければいけない状況ということ。
+	
 }
